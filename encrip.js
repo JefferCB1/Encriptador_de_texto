6763397433ba6d467d007da1647bcc2a30327b2f
JefferCB1 <carvajalberriojefferson@gmail.com>
2024-08-26 20:02:21 -0500

    Se añade separación de botones y desencriptar

--- encrip.js
+++ encrip.js
@@ -5,18 +5,45 @@ function encriptar(){
     let imagen = document.getElementById("imagen");
 
     let textoCifrado = texto
-      .replace(/a/gi, "ai")
-      .replace(/e/gi, "enter")
-      .replace(/i/gi, "imes")
-      .replace(/o/gi, "ober")
-      .replace(/u/gi, "ufat");
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/a/gi, "ai")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
 
     if (texto.length != 0){
         document.getElementById("texto").value = textoCifrado;
         tituloMensaje.textContent = "Texto encriptado con éxito";
         parrafo.textContent = "";
-        imagen.getElementById("imagen").src = "./img/encriptado.jpg";
+        imagen.src = "./img/encriptado.jpg";
 
+    }else{
+        imagen.src = "./img/muñeco.png"
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+        parrafo.textContent = "Ingresa texto que deseas encriptar o desencriptar";
+        alert("Debes ingresar algún texto");
+    }
+}
+
+function desencriptar(){
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo_mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let imagen = document.getElementById("imagen");
+
+
+    let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+
+    if (texto.length != 0){
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        imagen.src = "./img/desencriptado.jpg";
     }else{
         imagen.src = "./img/muñeco.png"
         tituloMensaje.textContent = "Ningún mensaje fue encontrado";
